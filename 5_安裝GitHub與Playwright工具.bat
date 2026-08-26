709af619d8610cd53201f896bc2594e764395cb4
ANDY <tpc0941101090@gmail.com>
2026-08-26 23:34:06 +0800

    deploy: 乾淨靜態網頁發布至 GitHub Pages

--- 5_安裝GitHub與Playwright工具.bat
+++
@@ -1,7 +0,0 @@
-﻿@echo off
-chcp 65001 >nul
-title [5] 安裝開發者工具與 Playwright
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\setup_developer_tools.ps1" -Execute
-echo.
-pause
